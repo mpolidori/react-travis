c66ff16573838f29f95d61ec8fbd57c251738f7f
Michael Polidori <spatiumestabsolutum@gmail.com>
2020-07-02 20:10:48 -0400

    KFLDJFLKDSF

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,14 +1,17 @@
-#!/bin/sh -e
+#!/bin/bash
+set -e
 
-echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
-sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
-sudo service jetty restart
+if [ $CKANVERSION == 'master' ]
+then
+   export CKAN_MINOR_VERSION=100
+else
+   export CKAN_MINOR_VERSION=${CKANVERSION##*.}
+fi
 
-nosetests --ckan \
-          --nologcapture \
-          --with-pylons=subdir/test.ini \
-          --with-coverage \
-          --cover-package=ckanext.react_usmetadata \
-          --cover-inclusive \
-          --cover-erase \
-          --cover-tests
+
+if (( $CKAN_MINOR_VERSION >= 9 ))
+then
+   pytest --ckan-ini=subdir/test.ini ckanext/react_usmetadata/tests
+else
+   nosetests --ckan --nologcapture --with-pylons=subdir/test.ini --cover-package=ckanext.react_usmetadata --cover-inclusive --cover-erase --cover-tests
+fi
