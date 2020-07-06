60f6e465fc3b90ad653cb7fe59ea9441fe7ede1a
Michael Polidori <spatiumestabsolutum@gmail.com>
2020-07-06 17:15:25 -0400

    Add tests and new paths

--- ckanext/react_usmetadata/plugin.py
+++ ckanext/react_usmetadata/plugin.py
@@ -1,11 +1,27 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+
 class React_UsmetadataPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IRoutes)
 
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
+
+    # IRoutes
+    def before_map(self, map):
+        controller = 'ckanext.react_usmetadata.controller:CustomPageController'
+
+        map.connect('/dataset/beta/new',
+                    controller=controller,
+                    action='retrieve_react_new')
+        map.connect('/dataset/beta/edit/{dataset_id}',
+                    controller=controller,
+                    action='retrieve_react_edit')
+        return map
+
+    def after_map(self, map):
+        return map
