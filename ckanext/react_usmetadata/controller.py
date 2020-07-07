213179292e9d4767501f0a9d195cde7226f4c86f
Michael Polidori <spatiumestabsolutum@gmail.com>
2020-07-07 11:32:16 -0400

    Add dataset check for edits

--- ckanext/react_usmetadata/controller.py
+++ ckanext/react_usmetadata/controller.py
@@ -2,14 +2,18 @@ from ckan.lib import base
 from ckan.controllers.package import PackageController as CorePackageController
 from ckan import model
 from ckan.plugins import toolkit
+from ckan.logic import NotFound
 
 
 class CustomPageController(CorePackageController):
+    def get_context(self):
+        return {'model': model, 'session': model.Session,
+                'user': toolkit.c.user,
+                'auth_user_obj': toolkit.c.userobj}
+
     def check_dataset_access(self):
         try:
-            context = {'model': model, 'session': model.Session,
-                       'user': toolkit.c.user,
-                       'auth_user_obj': toolkit.c.userobj}
+            context = self.get_context()
             toolkit.check_access('package_create', context)
         except toolkit.NotAuthorized:
             toolkit.abort(401,
@@ -21,5 +25,14 @@ class CustomPageController(CorePackageController):
 
     def retrieve_react_edit(self, dataset_id):
         self.check_dataset_access()
-        return base.render('package/beta_edit.html',
-                           extra_vars={'dataset_id': dataset_id})
+
+        try:
+            context = self.get_context()
+            toolkit.get_action(
+                'package_show')(context, {'name_or_id': dataset_id})
+            extra_vars = {'dataset_id': dataset_id}
+            return base.render('package/beta_edit.html',
+                               extra_vars=extra_vars)
+
+        except NotFound:
+            toolkit.abort(404, toolkit._('Dataset not found.'))
